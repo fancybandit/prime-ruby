154d3b3b06567a5661eb00b24ab56b50f071c38d
Jacob Klimenko <droidedjake@gmail.com>
2020-10-25 22:59:15 -0400

    commit tree 4ea561cda5e1eade6e1f60ed916203afc7db6d2c

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+
+def prime?(num)
+    i = 2
+    if i == num
+        true
+    elsif i > num
+        false
+    elsif i < num
+        lesser_values = (i...num).to_a
+        lesser_values.none? do |value|
+            num % value == 0
+        end
+    else
+        nil
+    end
+end
+
+
